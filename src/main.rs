0fbaae943bb652693c3fba35910e9ffb102af8b3
todd <todd_miller@skiplist.com>
2022-12-11 17:07:22 -0500

    added some better print statements

--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,12 @@ fn calculator(content: Vec<String>) -> Vec<i32> {
 
 fn main() {
     let lines = lines_from_file("inputs/01/input.txt").expect("could not read lines");
-    let mut elf_totals = calculator(lines);
-    let top_elves = elf_totals.drain(..3); 
+    let elf_totals = calculator(lines);
+    let top_elves = elf_totals.to_vec().drain(..3).sum::<i32>(); 
+    let top_elf = elf_totals.iter().max().unwrap();
     // need to sum the first three items in the vec elf_totals
-    println!("{}", top_elves.sum::<i32>())
+    println!("Most Calories held: {}", top_elf);
+    println!("Calories held by top three elves {}", top_elves);
     
 }
 
