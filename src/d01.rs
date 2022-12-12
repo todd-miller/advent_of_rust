88550ed9d9790f90b60976fb9970ce915f1ac7be
Todd Miller <todd_miller@skiplist.com>
2022-12-12 17:00:02 -0500

    adapted directory structure and added LSP support for rust

---
+++ src/d01.rs
@@ -0,0 +1,37 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
+    BufReader::new(File::open(filename)?).lines().collect()
+}
+
+fn calculator(content: Vec<String>) -> Vec<i32> {
+    let mut totals = vec![];
+    let mut count:i32 = 0;
+ 
+    for line in content {
+        if !line.is_empty() {
+            let num:i32 = line.parse().unwrap();
+            count = count + num; 
+        } else {
+            totals.push(count);
+            count = 0;
+        }
+    }
+    totals.sort();
+    totals.reverse();
+    totals
+}
+
+pub fn day1() {
+    let lines = lines_from_file("inputs/01/input.txt").expect("could not read lines");
+    let elf_totals = calculator(lines);
+    let top_elves = elf_totals.to_vec().drain(..3).sum::<i32>(); 
+    let top_elf = elf_totals.iter().max().unwrap();
+
+    println!("Most Calories held: {}", top_elf);
+    println!("Calories held by top three elves {}", top_elves); 
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,8 @@
-use std::{
-    fs::File,
-    io::{self, BufRead, BufReader},
-    path::Path,
-};
+mod d01;
 
-fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
-    BufReader::new(File::open(filename)?).lines().collect()
-}
-
-fn calculator(content: Vec<String>) -> Vec<i32> {
-    let mut totals = vec![];
-    let mut count:i32 = 0;
- 
-    for line in content {
-        if !line.is_empty() {
-            let num:i32 = line.parse().unwrap();
-            count = count + num; 
-        } else {
-            totals.push(count);
-            count = 0;
-        }
-    }
-    totals.sort();
-    totals.reverse();
-    totals
-}
+use crate::d01::*;
 
 fn main() {
-    let lines = lines_from_file("inputs/01/input.txt").expect("could not read lines");
-    let elf_totals = calculator(lines);
-    let top_elves = elf_totals.to_vec().drain(..3).sum::<i32>(); 
-    let top_elf = elf_totals.iter().max().unwrap();
-    // need to sum the first three items in the vec elf_totals
-    println!("Most Calories held: {}", top_elf);
-    println!("Calories held by top three elves {}", top_elves); 
+    day1();
 }
 
