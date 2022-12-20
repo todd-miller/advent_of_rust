5e3d98e24b18489af509ad8208088d8d4e32b1ec
Todd Miller <todd_miller@skiplist.com>
2022-12-20 11:59:41 -0500

    DAY04 start

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 mod d01;
 mod d02;
 mod d03;
+mod d04;
 mod utils;
 
 use crate::d01::*;
 use crate::d02::*;
 use crate::d03::*;
+use crate::d04::*;
 
 fn main() {
     day1();
     day2();
     day3();
+    day4();
 }
 
